323e219b5dcb12485938b23f41c851ef892d07dc
Pratik1968 <47908854+Pratik1968@users.noreply.github.com>
2024-01-18 18:47:16 +0530

    added static file serving

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,6 +6,7 @@ dotenv.config();
 let Project:Array<JSON> ;
 const app: Express = express();
 const port = process.env.PORT || 3000;
+app.use(express.static("../../my-portfolio/dist"))
 app.get("/getProjects", (req: Request, res: Response) => {
     fs.readFile(process.env.PROJECT_PATH,"utf-8",(error:Error,result:string)=>{
         if(error){
